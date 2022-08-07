687cb136a19812077c7f69f8ffea0d5bdf34e0da
electrojam <electrojam@hotmail.com>
2022-08-07 11:11:35 -0500

    consulta api fetch server local

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList, Text } from 'react-native'
 
-import repositories from '../data/repositories.js'
 import RepositoryItem from './RepositoryItem.jsx'
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState(null)
+
+  const fetchRepositories = async () => {
+    const response = await globalThis.fetch('http://192.168.1.8:5000/api/repositories')
+    const json = await response.json()
+    setRepositories(json)
+  }
+
+  useEffect(() => {
+    fetchRepositories()
+  }, [])
+  
+  const repositoriesNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : []
+
   return (
     <FlatList 
-        data={repositories}
+        data={repositoriesNodes}
         ItemSeparatorComponent={() => <Text> </Text>}
         renderItem={({ item: repo }) => (
             <RepositoryItem {...repo} />
