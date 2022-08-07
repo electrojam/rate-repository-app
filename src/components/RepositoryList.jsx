0a01a98c14d37b5eaaefcd86fedd335cba3d2224
electrojam <electrojam@hotmail.com>
2022-08-07 11:36:16 -0500

    fetch en un custom hook

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { FlatList, Text } from 'react-native'
+import useRepositories from '../hooks/useRepositories.js'
 
 import RepositoryItem from './RepositoryItem.jsx'
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState(null)
-
-  const fetchRepositories = async () => {
-    const response = await globalThis.fetch('http://192.168.1.8:5000/api/repositories')
-    const json = await response.json()
-    setRepositories(json)
-  }
-
-  useEffect(() => {
-    fetchRepositories()
-  }, [])
-  
-  const repositoriesNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
-    : []
+    const { repositories } = useRepositories()
 
   return (
     <FlatList 
-        data={repositoriesNodes}
+        data={repositories}
         ItemSeparatorComponent={() => <Text> </Text>}
         renderItem={({ item: repo }) => (
             <RepositoryItem {...repo} />
